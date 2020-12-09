275873c9d7885a9e4b07f2dc0317d703a6f56e65
Sage Hane <sage@sagehane.com>
2020-12-09 19:55:25 +0900

    Check for file size

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use pomeg_editor_cli::is_valid_save;
+use std::fs;
 use std::io::Read;
 
 fn main() {
     let file = std::env::args().nth(1).expect("no file given"); // The program expects a file for an argument
     let mut buffer = [0; 0x20000];
+
+    if fs::metadata(&file).unwrap().len() != 0x20000 {
+        eprintln!("Invalid file size, should be 128 KiB");
+
+        std::process::exit(1)
+    }
 
     std::fs::File::open(file)
         .unwrap()
