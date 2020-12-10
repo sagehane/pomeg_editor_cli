3579bb01ac1b99d3827b00732eb3ed20bc5c3112
Sage Hane <sage@sagehane.com>
2020-12-10 10:26:57 +0900

    Rework the relations between buffers and sectors

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,21 @@ const SECTOR_SIZE: [u16; 14] = [
     3884, 3968, 3968, 3968, 3848, 3968, 3968, 3968, 3968, 3968, 3968, 3968, 3968, 2000,
 ];
 
+/// Take an buffer and id to return a sector in the form of a slice
+///
+/// # Panics
+///
+/// Panics when `sector_id > 31`
+fn buffer_to_sector(sector_id: u8, buffer: &[u8]) -> &[u8] {
+    if sector_id > 31 {
+        panic!("Sector must be between 0 and 31")
+    }
+
+    let sector_offset: usize = (sector_id as usize) << 12;
+
+    &buffer[sector_offset..sector_offset + 0x1000]
+}
+
 /// Separates a slice into litte-endian u32 values, gets the total sum of the slice, and gets the
 /// sum of the upper and lower 16 bytes.
 fn calculate_checksum(sector: &[u8]) -> u16 {
@@ -18,17 +33,11 @@ fn calculate_checksum(sector: &[u8]) -> u16 {
 
 /// Checks if the checksum of a sector is valid by comparing the value of the two bytes stored in
 /// offset of 0xff4 with the value from `calculate_checksum`.
-fn is_valid_sector(sector_id: u8, buffer: &[u8]) -> bool {
-    let sector_offset: usize = (sector_id as usize) << 12;
+fn is_valid_sector(sector: &[u8]) -> bool {
+    let calculated_checksum =
+        calculate_checksum(&sector[..SECTOR_SIZE[*&sector[0xFF4] as usize] as usize]);
 
-    let calculated_checksum = calculate_checksum(
-        &buffer[sector_offset
-            ..sector_offset + SECTOR_SIZE[*&buffer[(sector_offset) + 0xff4] as usize] as usize],
-    );
-
-    let checksum = LittleEndian::read_u16(
-        &buffer[(sector_offset) + 0xff6 as usize..(sector_offset) + 0xff8 as usize],
-    );
+    let checksum = LittleEndian::read_u16(&sector[0xFF6..0xFF8]);
 
     if calculated_checksum != checksum {
         return false;
@@ -38,11 +47,11 @@ fn is_valid_sector(sector_id: u8, buffer: &[u8]) -> bool {
 }
 
 /// Checks if the save file has the correct checksum. Currently only checks through sectors 0 to
-/// 28, leaving 29 to 31 unchecked.
+/// 27, leaving 28 to 31 unchecked.
 pub fn is_valid_save(buffer: &[u8]) -> bool {
-    for i in 0..28 {
-        if !is_valid_sector(i, buffer) {
-            eprintln!("Sector {} has an invalid checksum", i);
+    for sector_id in 0..27 {
+        if !is_valid_sector(buffer_to_sector(sector_id, buffer)) {
+            eprintln!("Sector {} has an invalid checksum", sector_id);
 
             return false;
         }
